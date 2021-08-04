7848645f7c2bd0a61c418d66c080084f317f0031
Fendo181 <fendo181git@gmail.com>
2021-08-04 09:58:30 +0900

    p14 2.2.1 全てがオブジェクト

---
+++ cherry_ruby/chap02/object_sample.rb
@@ -0,0 +1,17 @@
+# rubyでは全てがオブジェクトになる為、メソッドが呼び出させる。
+# nilやtrue、falseに対してもオブジェクトになりメソッドを呼び出す事ができる!
+
+# 文字列
+puts '1'.to_s
+
+# 数値
+puts 1.to_s
+
+# nil
+puts nil.to_s
+
+# true
+puts true.to_s
+
+# false
+puts false.to_s
