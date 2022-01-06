612756e6df173978c67ac4a037b5cd2ac49d95a8
endu <futoshi.endo@pepabo.com>
2022-01-06 09:27:19 +0900

    2.5.1 Rubyの真偽値

---
+++ cherry_ruby/chap02/true_false.rb
@@ -0,0 +1,19 @@
+if 0
+    puts true 
+else 
+    puts false
+end 
+
+data = 'find_data'
+if data != nil
+    puts 'データがあります'
+else
+    puts 'データがありません'
+end
+
+not_data = nil
+if not_data
+  puts 'データがあります'
+else
+  puts 'データがありません'
+end
