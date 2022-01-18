8b7ddca531216766ba3b06c077079f87f238dc10
endu <futoshi.endo@pepabo.com>
2022-01-19 08:32:15 +0900

    2.7 fizz_buzzのコードを追加
    これだとfizz_buzzがでない

---
+++ cherry_ruby/chap02/fizz_buzz.rb
@@ -0,0 +1,27 @@
+def fizz_buzz(num)
+  if num%3 == 0
+    'fizz'
+  elsif num%5 == 0
+    'buzz'
+  elsif num%15 == 0
+    'fizz_buzz'
+  else
+    num.to_s
+  end
+end
+
+puts fizz_buzz(1)
+puts fizz_buzz(2)
+puts fizz_buzz(3)
+puts fizz_buzz(4)
+puts fizz_buzz(5)
+puts fizz_buzz(6)
+puts fizz_buzz(7)
+puts fizz_buzz(8)
+puts fizz_buzz(9)
+puts fizz_buzz(10)
+puts fizz_buzz(11)
+puts fizz_buzz(12)
+puts fizz_buzz(13)
+puts fizz_buzz(14)
+puts fizz_buzz(15)
